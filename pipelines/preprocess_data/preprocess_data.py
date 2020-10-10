a6b28c42cf634f4eecfb43348591bc139add12b1
Mugumya Twarik Harouna <51790725+twarik@users.noreply.github.com>
2020-10-11 00:32:29 +0300

    Update preprocess_data.py

--- pipelines/preprocess_data/preprocess_data.py
+++ pipelines/preprocess_data/preprocess_data.py
@@ -1,16 +1,46 @@
+import pandas as pd
 import numpy as np
-from sklearn import datasets
 from sklearn.model_selection import train_test_split
-def _preprocess_data():
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import StratifiedKFold
+from sklearn.feature_selection import RFECV
+from sklearn.preprocessing import MinMaxScaler
+
+def _preprocess_data(dataset):
     '''
-    This component will
-    1. Load the presidential elections datasets
-    2. Clean and transoform the datasets
-    3. Split the dataset into train and test sets
+    This function/component will:
+    1. Load the presidential elections dataset
+    2. Clean and transform the dataset
+    3. Split the dataset into train and test set
     4. Use np.save to save our dataset to disk so that it can be reused by later components
     '''
-    X, y = datasets.load_boston(return_X_y=True)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
+    df = pd.read_csv(dataset)
+    df.drop(['notes','state_po','candidate', 'office','state','writein','state_ic'], axis = 1,inplace=True )
+    df.dropna(inplace=True)
+
+    X = df.drop('party', axis=1)
+    target = df['party']
+
+    rfc = RandomForestClassifier(random_state=101)
+    rfecv = RFECV(estimator=rfc, step=1, cv=StratifiedKFold(10), scoring='accuracy')
+    rfecv.fit(X, target)
+
+    X.drop(X.columns[np.where(rfecv.support_ == False)[0]], axis=1, inplace=True)
+    y = target
+
+    #split the data
+    x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+
+    #Scaling the data
+    scaler = MinMaxScaler()
+    normalised_train_df = scaler.fit_transform(x_train)
+    normalised_train_df = pd.DataFrame(normalised_train_df, columns=x_train.columns)
+
+    normalised_test_df = scaler.transform(x_test)
+    normalised_test_df = pd.DataFrame(normalised_test_df, columns=x_train.columns)
+
+    #split the data
+    X_train, X_test = normalised_train_df, normalised_test_df
     np.save('x_train.npy', X_train)
     np.save('x_test.npy', X_test)
     np.save('y_train.npy', y_train)
